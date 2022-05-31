d28bbff4aa7a13a024a7e09e09108919b96b20ce
Nguyen Hai Nam <namnh18@vng.com.vn>
2022-05-31 21:49:53 +0700

    Tutorial 1

--- OpenGLApp/Main.cpp
+++ OpenGLApp/Main.cpp
@@ -2,18 +2,40 @@
 #include <glew.h>
 #include <glut.h>
 
-int main(int argc, char* argv[]) {
-	printf("Hi hi");
+void RenderSceneCB() {
+	static GLclampf c = 0.0f;
+	glClearColor(c, c, c, c);
 
+	c += 1.0f / 256.0f / 50.0f;
+	if (c >= 1.0f)c = 0.0f;
+
+	//glClear is a Command while glClearColor is State Setting: which sets the color value to buffer.glClearColor is State Setting: which sets the color value to buffer.
+	glClear(GL_COLOR_BUFFER_BIT);
+	//force this function to be called again and again!
+	glutPostRedisplay();
+	//swap the role of front and back buffur from using GLUT_DOUBLE
+	glutSwapBuffers();
+}
+
+int main(int argc, char** argv) {
 	glutInit(&argc, argv);
-	glutCreateWindow("GLEW Test");
-	GLenum err = glewInit();
-	if (GLEW_OK != err)
-	{
-		/* Problem: glewInit failed, something is seriously wrong. */
-		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-	}
-	fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
-	getchar();
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+
+	int width = 1920 / 2, height = 1080 / 2;
+	glutInitWindowSize(width, height);
+
+	int x = 200, y = 100;
+	glutInitWindowPosition(x, y);
+
+	int win = glutCreateWindow("Tutorial");
+	printf("window id: %d\n", win);
+
+	GLclampf Red = 0.0f, Green = 1.0f, Blue = 0.0f, Alpha = 0.0f;
+	glClearColor(Red, Green, Blue, Alpha);
+
+	glutDisplayFunc(RenderSceneCB);
+
+	glutMainLoop();
+
 	return 0;
 }
