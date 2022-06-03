471d82683d72c34188fe7b7e6754720349341bfa
Nguyen Hai Nam <namnh18@vng.com.vn>
2022-06-03 19:04:19 +0700

    Tutorial 3

--- OpenGLApp/Main.cpp
+++ OpenGLApp/Main.cpp
@@ -1,19 +1,52 @@
 #include <stdio.h>
 #include <glew.h>
 #include <glut.h>
+#include <ogldev_math_3d.h>
+
+GLuint bufID;
+
+//Creating VBOs
+void CreateVertexBuffer() {
+	Vector3f Vertices[3];
+	Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f);
+	Vertices[1] = Vector3f( 0.0f,  1.0f, 0.0f);
+	Vertices[2] = Vector3f( 1.0f, -1.0f, 0.0f);
+
+	//generating an unique identifier
+	//creates the buffer object, storing the handle to the object in the variable bufID
+	glGenBuffers(1, &bufID);
+	printf("buffer id: %d\n", bufID);
+
+	//tell the OpenGL driver that we plan to populate it with vertex attribute
+	//binds the newly-created buffer object to the GL_ARRAY_BUFFER binding target
+	glBindBuffer(GL_ARRAY_BUFFER, bufID);
+	//allocates memory for the buffer currently bound to GL_ARRAY_BUFFER
+	//transfer the vertex data (the position in this case) into the vertex buffer object
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+	//unbound, cleanup
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
 
 void RenderSceneCB() {
-	static GLclampf c = 0.0f;
-	glClearColor(c, c, c, c);
-
-	c += 1.0f / 256.0f / 50.0f;
-	if (c >= 1.0f)c = 0.0f;
-
-	//glClear is a Command while glClearColor is State Setting: which sets the color value to buffer.glClearColor is State Setting: which sets the color value to buffer.
 	glClear(GL_COLOR_BUFFER_BIT);
-	//force this function to be called again and again!
-	glutPostRedisplay();
-	//swap the role of front and back buffur from using GLUT_DOUBLE
+
+	glBindBuffer(GL_ARRAY_BUFFER, bufID);
+
+	//allow vertex attribute at index 0 to be modified
+	//layout(location = 0) in vec4 position;//code in vertex shader
+	glEnableVertexAttribArray(0);
+
+	//define an array of generic vertex attr at index 0 with 3 element of FLOATs
+	//without normalize data to [0,1] or [-1,1], without offset between consecutive
+	//generic vertex attributes
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	//Rendering using VBOs
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glDisableVertexAttribArray(0);
+
+	//swap the role of front and back buffer from using GLUT_DOUBLE
 	glutSwapBuffers();
 }
 
@@ -27,11 +60,24 @@ int main(int argc, char** argv) {
 	int x = 200, y = 100;
 	glutInitWindowPosition(x, y);
 
-	int win = glutCreateWindow("Tutorial");
+	int win = glutCreateWindow("Tutorial 02");
 	printf("window id: %d\n", win);
 
-	GLclampf Red = 0.0f, Green = 1.0f, Blue = 0.0f, Alpha = 0.0f;
+	GLenum res = glewInit();
+	if (res != GLEW_OK) {
+		fprintf(stderr, "Error: '%s' \n", glewGetErrorString(res));
+		return 1;
+	}
+
+	GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
 	glClearColor(Red, Green, Blue, Alpha);
+
+	//Setup face
+	glEnable(GL_CULL_FACE);
+	glFrontFace(GL_CW);
+	//glCullFace(GL_FRONT);
+
+	CreateVertexBuffer();
 
 	glutDisplayFunc(RenderSceneCB);
 
---
+++ include/ogldev_math_3d.h
@@ -0,0 +1,276 @@
+/*
+        Copyright 2010 Etay Meiri
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MATH_3D_H
+#define MATH_3D_H
+
+#ifdef _WIN64
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#endif
+#include <cmath>
+#else
+#include <math.h>
+#endif
+#include <stdio.h>
+
+
+#define ToRadian(x) (float)(((x) * M_PI / 180.0f))
+#define ToDegree(x) (float)(((x) * 180.0f / M_PI))
+
+float RandomFloat();
+
+struct Vector2i
+{
+    int x;
+    int y;
+};
+
+struct Vector2f
+{
+    float x;
+    float y;
+
+    Vector2f()
+    {
+    }
+
+    Vector2f(float _x, float _y)
+    {
+        x = _x;
+        y = _y;
+    }
+};
+
+struct Vector4f;
+
+struct Vector3f
+{
+    union {
+        float x;
+        float r;
+    };
+
+    union {
+        float y;
+        float g;
+    };
+
+    union {
+        float z;
+        float b;
+    };
+
+    Vector3f() {}
+
+    Vector3f(float _x, float _y, float _z)
+    {
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+
+    Vector3f(const float* pFloat)
+    {
+        x = pFloat[0];
+        y = pFloat[1];
+        z = pFloat[2];
+    }
+
+    Vector3f(float f)
+    {
+        x = y = z = f;
+    }
+
+    Vector3f(const Vector4f& v);
+
+    Vector3f& operator+=(const Vector3f& r)
+    {
+        x += r.x;
+        y += r.y;
+        z += r.z;
+
+        return *this;
+    }
+
+    Vector3f& operator-=(const Vector3f& r)
+    {
+        x -= r.x;
+        y -= r.y;
+        z -= r.z;
+
+        return *this;
+    }
+
+    Vector3f& operator*=(float f)
+    {
+        x *= f;
+        y *= f;
+        z *= f;
+
+        return *this;
+    }
+
+    bool operator==(const Vector3f& r)
+    {
+        return ((x == r.x) && (y == r.y) && (z == r.z));
+    }
+
+    bool operator!=(const Vector3f& r)
+    {
+        return !(*this == r);
+    }
+
+    operator const float* () const
+    {
+        return &(x);
+    }
+
+
+    Vector3f Cross(const Vector3f& v) const;
+
+    float Dot(const Vector3f& v) const
+    {
+        float ret = x * v.x + y * v.y + z * v.z;
+        return ret;
+    }
+
+    float Distance(const Vector3f& v) const
+    {
+        float delta_x = x - v.x;
+        float delta_y = y - v.y;
+        float delta_z = z - v.z;
+        float distance = sqrtf(delta_x * delta_x + delta_y * delta_y + delta_z * delta_z);
+        return distance;
+    }
+
+    float Length() const
+    {
+        float len = sqrtf(x * x + y * y + z * z);
+        return len;
+    }
+
+    bool IsZero() const
+    {
+        return ((x + y + z) == 0.0f);
+    }
+
+    Vector3f& Normalize();
+
+    void Rotate(float Angle, const Vector3f& Axis);
+
+    Vector3f Negate() const;
+
+    void Print(bool endl = true) const
+    {
+        printf("(%f, %f, %f)", x, y, z);
+
+        if (endl) {
+            printf("\n");
+        }
+    }
+};
+
+
+struct Vector4f
+{
+    float x;
+    float y;
+    float z;
+    float w;
+
+    Vector4f()
+    {
+    }
+
+    Vector4f(float _x, float _y, float _z, float _w)
+    {
+        x = _x;
+        y = _y;
+        z = _z;
+        w = _w;
+    }
+
+    Vector4f(const Vector3f& v, float _w)
+    {
+        x = v.x;
+        y = v.y;
+        z = v.z;
+        w = _w;
+    }
+
+    void Print(bool endl = true) const
+    {
+        printf("(%f, %f, %f, %f)", x, y, z, w);
+
+        if (endl) {
+            printf("\n");
+        }
+    }
+
+    Vector3f to3f() const
+    {
+        Vector3f v(x, y, z);
+        return v;
+    }
+};
+
+
+
+inline Vector3f operator+(const Vector3f& l, const Vector3f& r)
+{
+    Vector3f Ret(l.x + r.x,
+        l.y + r.y,
+        l.z + r.z);
+
+    return Ret;
+}
+
+inline Vector3f operator-(const Vector3f& l, const Vector3f& r)
+{
+    Vector3f Ret(l.x - r.x,
+        l.y - r.y,
+        l.z - r.z);
+
+    return Ret;
+}
+
+inline Vector3f operator*(const Vector3f& l, float f)
+{
+    Vector3f Ret(l.x * f,
+        l.y * f,
+        l.z * f);
+
+    return Ret;
+}
+
+inline Vector3f::Vector3f(const Vector4f& v)
+{
+    x = v.x;
+    y = v.y;
+    z = v.z;
+}
+
+inline Vector4f operator/(const Vector4f& l, float f)
+{
+    Vector4f Ret(l.x / f,
+        l.y / f,
+        l.z / f,
+        l.w / f);
+
+    return Ret;
+}
+
+
+#endif  /* MATH_3D_H */
