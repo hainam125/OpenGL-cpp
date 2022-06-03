9bf42bbf95e486cdab71d9bbef802984e5b5ab30
Nguyen Hai Nam <namnh18@vng.com.vn>
2022-06-03 20:14:27 +0700

    Refactor

--- OpenGLApp/Main.cpp
+++ OpenGLApp/Main.cpp
@@ -5,25 +5,15 @@
 
 GLuint bufID;
 
-//Creating VBOs
 void CreateVertexBuffer() {
 	Vector3f Vertices[3];
 	Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f);
 	Vertices[1] = Vector3f( 0.0f,  1.0f, 0.0f);
 	Vertices[2] = Vector3f( 1.0f, -1.0f, 0.0f);
 
-	//generating an unique identifier
-	//creates the buffer object, storing the handle to the object in the variable bufID
 	glGenBuffers(1, &bufID);
-	printf("buffer id: %d\n", bufID);
-
-	//tell the OpenGL driver that we plan to populate it with vertex attribute
-	//binds the newly-created buffer object to the GL_ARRAY_BUFFER binding target
 	glBindBuffer(GL_ARRAY_BUFFER, bufID);
-	//allocates memory for the buffer currently bound to GL_ARRAY_BUFFER
-	//transfer the vertex data (the position in this case) into the vertex buffer object
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-	//unbound, cleanup
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
@@ -32,36 +22,20 @@ void RenderSceneCB() {
 
 	glBindBuffer(GL_ARRAY_BUFFER, bufID);
 
-	//allow vertex attribute at index 0 to be modified
-	//layout(location = 0) in vec4 position;//code in vertex shader
 	glEnableVertexAttribArray(0);
-
-	//define an array of generic vertex attr at index 0 with 3 element of FLOATs
-	//without normalize data to [0,1] or [-1,1], without offset between consecutive
-	//generic vertex attributes
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-	//Rendering using VBOs
 	glDrawArrays(GL_TRIANGLES, 0, 3);
-
 	glDisableVertexAttribArray(0);
 
-	//swap the role of front and back buffer from using GLUT_DOUBLE
 	glutSwapBuffers();
 }
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-
-	int width = 1920 / 2, height = 1080 / 2;
-	glutInitWindowSize(width, height);
-
-	int x = 200, y = 100;
-	glutInitWindowPosition(x, y);
-
-	int win = glutCreateWindow("Tutorial 02");
-	printf("window id: %d\n", win);
+	glutInitWindowSize(1920 / 2, 1080 / 2);
+	glutInitWindowPosition(200, 100);
+	glutCreateWindow("OpenGL");
 
 	GLenum res = glewInit();
 	if (res != GLEW_OK) {
@@ -69,10 +43,8 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
-	GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
-	glClearColor(Red, Green, Blue, Alpha);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
-	//Setup face
 	glEnable(GL_CULL_FACE);
 	glFrontFace(GL_CW);
 	//glCullFace(GL_FRONT);
@@ -80,7 +52,6 @@ int main(int argc, char** argv) {
 	CreateVertexBuffer();
 
 	glutDisplayFunc(RenderSceneCB);
-
 	glutMainLoop();
 
 	return 0;
