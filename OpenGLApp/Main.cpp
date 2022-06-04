eeb509763402cbafc23d2d7bc4dfd38e66edea7b
Nguyen Hai Nam <namnh18@vng.com.vn>
2022-06-04 09:30:44 +0700

    Tutorial 4

--- OpenGLApp/Main.cpp
+++ OpenGLApp/Main.cpp
@@ -1,20 +1,106 @@
 #include <stdio.h>
 #include <glew.h>
 #include <glut.h>
-#include <ogldev_math_3d.h>
+#include <string>
+#include <vector>
+#include <helper.h>
 
 GLuint bufID;
 
 void CreateVertexBuffer() {
-	Vector3f Vertices[3];
-	Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f);
-	Vertices[1] = Vector3f( 0.0f,  1.0f, 0.0f);
-	Vertices[2] = Vector3f( 1.0f, -1.0f, 0.0f);
+	float Vertices[] = {
+		-1.0f, -1.0f, 0.0f, 1.0f,
+		 0.0f,  1.0f, 0.0f, 1.0f,
+		 1.0f, -1.0f, 0.0f, 1.0f,
+	};
 
 	glGenBuffers(1, &bufID);
 	glBindBuffer(GL_ARRAY_BUFFER, bufID);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+GLuint CreateShader(GLenum eShaderType, const std::string& strShaderFile) {
+	const char* strFileData = strShaderFile.c_str();
+
+	GLuint shader = glCreateShader(eShaderType);
+	//set data for shader object
+	glShaderSource(shader, 1, &strFileData, NULL);
+
+	glCompileShader(shader);
+
+	GLint status;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+	if (status == GL_FALSE) {
+		GLint infoLogLength;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
+		GLchar* strInfoLog = new GLchar[infoLogLength + 1];
+		glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
+
+		const char* strShaderType = NULL;
+		switch (eShaderType)
+		{
+		case GL_VERTEX_SHADER: strShaderType = "vertex"; break;
+		case GL_GEOMETRY_SHADER: strShaderType = "geometry"; break;
+		case GL_FRAGMENT_SHADER: strShaderType = "fragment"; break;
+		}
+
+		fprintf(stderr, "Compile failure in %s shader:\n%s\n", strShaderType, strInfoLog);
+		delete[] strInfoLog;
+		exit(1);
+	}
+
+	return shader;
+}
+
+GLuint CreateProgram(const std::vector<GLuint>& shaderList) {
+	GLuint program = glCreateProgram();
+	for (size_t i = 0; i < shaderList.size(); i++) {
+		glAttachShader(program, shaderList[i]);
+	}
+	glLinkProgram(program);
+
+	GLint status;
+	glGetProgramiv(program, GL_LINK_STATUS, &status);
+	if (status == GL_FALSE) {
+		GLint infoLogLength;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
+		GLchar* strInfoLog = new GLchar[infoLogLength + 1];
+		glGetProgramInfoLog(program, infoLogLength, NULL, strInfoLog);
+		fprintf(stderr, "Link failure: %s\n", strInfoLog);
+		delete[]strInfoLog;
+		exit(1);
+	}
+
+	glValidateProgram(program);
+	glGetProgramiv(program, GL_VALIDATE_STATUS, &status);
+	if (status == GL_FALSE) {
+		GLint infoLogLength;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
+		GLchar* strInfoLog = new GLchar[infoLogLength + 1];
+		glGetProgramInfoLog(program, infoLogLength, NULL, strInfoLog);
+		fprintf(stderr, "Validate failure: %s\n", strInfoLog);
+		delete[]strInfoLog;
+		exit(1);
+	}
+
+	for (size_t i = 0; i < shaderList.size(); i++) {
+		glDetachShader(program, shaderList[i]);
+	}
+
+	return program;
+}
+
+void InitializeProgram() {
+	std::vector<GLuint> shaderList;
+	shaderList.push_back(CreateShader(GL_VERTEX_SHADER, get_file_contents("shader.vs")));
+	shaderList.push_back(CreateShader(GL_FRAGMENT_SHADER, get_file_contents("shader.fs")));
+
+	GLint program = CreateProgram(shaderList);
+	for (size_t i = 0; i < shaderList.size(); i++) {
+		glDeleteShader(shaderList[i]);
+	}
+	glUseProgram(program);
 }
 
 void RenderSceneCB() {
@@ -23,7 +109,7 @@ void RenderSceneCB() {
 	glBindBuffer(GL_ARRAY_BUFFER, bufID);
 
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 	glDisableVertexAttribArray(0);
 
@@ -50,6 +136,7 @@ int main(int argc, char** argv) {
 	//glCullFace(GL_FRONT);
 
 	CreateVertexBuffer();
+	InitializeProgram();
 
 	glutDisplayFunc(RenderSceneCB);
 	glutMainLoop();
