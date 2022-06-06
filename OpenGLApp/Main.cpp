9704aa4f5570dcace72fc177de26139c1eafe388
Nguyen Hai Nam <namnh18@vng.com.vn>
2022-06-06 22:10:11 +0700

    Indexed Draws

--- OpenGLApp/Main.cpp
+++ OpenGLApp/Main.cpp
@@ -7,19 +7,87 @@
 #include <ogldev_math_3d.h>
 
 GLuint VBO;
+GLuint IBO;
 GLint gMatrixLocation;
 
+struct Vertex
+{
+	Vector3f pos;
+	Vector3f color;
+
+	Vertex(){}
+
+	Vertex(float x, float y) {
+		pos = Vector3f(x, y, 0.0f);
+		float r = (float)rand() / (float)RAND_MAX;
+		float g = (float)rand() / (float)RAND_MAX;
+		float b = (float)rand() / (float)RAND_MAX;
+		color = Vector3f(r, g, b);
+	}
+};
+
 void CreateVertexBuffer() {
-	float vertexData[] = {
-		-1.0f, -1.0f, 0.0f,
-		 0.0f,  1.0f, 0.0f,
-		 1.0f, -1.0f, 0.0f,
-	};
+	//rectangular
+	Vertex vertices[19];
+	//Center
+	vertices[0] = Vertex(0.0f, 0.0f);
+	//Top row
+	vertices[1] = Vertex(-1.00f, 1.0f);
+	vertices[2] = Vertex(-0.75f, 1.0f);
+	vertices[3] = Vertex(-0.50f, 1.0f);
+	vertices[4] = Vertex(-0.25f, 1.0f);
+	vertices[5] = Vertex( 0.00f, 1.0f);
+	vertices[6] = Vertex( 0.25f, 1.0f);
+	vertices[7] = Vertex( 0.50f, 1.0f);
+	vertices[8] = Vertex( 0.75f, 1.0f);
+	vertices[9] = Vertex( 1.00f, 1.0f);
+	//Bttom row
+	vertices[10] = Vertex(-1.00f, -1.0f);
+	vertices[11] = Vertex(-0.75f, -1.0f);
+	vertices[12] = Vertex(-0.50f, -1.0f);
+	vertices[13] = Vertex(-0.25f, -1.0f);
+	vertices[14] = Vertex( 0.00f, -1.0f);
+	vertices[15] = Vertex( 0.25f, -1.0f);
+	vertices[16] = Vertex( 0.50f, -1.0f);
+	vertices[17] = Vertex( 0.75f, -1.0f);
+	vertices[18] = Vertex( 1.00f, -1.0f);
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void CreateIndexBuffer() {
+	unsigned int indices[] = {
+		// Top triangles
+		0, 2, 1,
+		0, 3, 2,
+		0, 4, 3,
+		0, 5, 4,
+		0, 6, 5,
+		0, 7, 6,
+		0, 8, 7,
+		0, 9, 8,
+		// Bottom triangles
+		0, 10, 11,
+		0, 11, 12,
+		0, 12, 13,
+		0, 13, 14,
+		0, 14, 15,
+		0, 15, 16,
+		0, 16, 17,
+		0, 17, 18,
+		// Left triangle
+		0, 1, 10,
+		// Right triangle
+		0, 18, 9
+	};
+
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 GLuint CreateShader(GLenum eShaderType, const std::string& strShaderFile) {
@@ -113,7 +181,7 @@ void InitializeProgram() {
 }
 
 void Transformation() {
-	static float Scale = 0.25f;
+	static float Scale = 1.0f;
 
 	Matrix4f Scaling(Scale, 0.0f,  0.0f,  0.0f,
                      0.0f,  Scale, 0.0f,  0.0f,
@@ -123,14 +191,14 @@ void Transformation() {
 	static float AngleInRadians = 0.0f;
 	static float Delta = 0.0001f;
 
-	AngleInRadians += Delta;
+	//AngleInRadians += Delta;
 
 	Matrix4f Rotation(cosf(AngleInRadians), -sinf(AngleInRadians), 0.0f, 0.0f,
-                      sinf(AngleInRadians), cosf(AngleInRadians),  0.0f, 0.0f,
+                      sinf(AngleInRadians),  cosf(AngleInRadians), 0.0f, 0.0f,
                       0.0,                  0.0f,                  1.0f, 0.0f,
                       0.0f,                 0.0f,                  0.0f, 1.0f);
 
-	static float Loc = 0.5f;
+	static float Loc = 0.0f;
 
     Matrix4f Translation(1.0f, 0.0f, 0.0f, Loc,
                          0.0f, 1.0f, 0.0f, 0.0,
@@ -150,20 +218,35 @@ void RenderSceneCB() {
 	Transformation();
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
 
+	//position
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	//stride: 3 floats of position + 3 floats of color
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
+
+	//color
+	glEnableVertexAttribArray(1);
+	//offset: start after 3 floats of position
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+
+	//draw using index buffer: 18 triangle ~ 54 indices
+	glDrawElements(GL_TRIANGLES, 54, GL_UNSIGNED_INT, 0);
+
 	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
 
 	glutSwapBuffers();
 	glutPostRedisplay();
 }
 
 int main(int argc, char** argv) {
+	//set seed for random func
+	srand(GetCurrentProcessId());
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-	glutInitWindowSize(1080 / 2, 1080 / 2);
+	glutInitWindowSize(1920 / 2, 1080 / 2);
 	glutInitWindowPosition(200, 100);
 	glutCreateWindow("OpenGL");
 
@@ -176,10 +259,11 @@ int main(int argc, char** argv) {
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
 	glEnable(GL_CULL_FACE);
-	glFrontFace(GL_CW);
+	glFrontFace(GL_CCW);
 	glCullFace(GL_BACK);
 
 	CreateVertexBuffer();
+	CreateIndexBuffer();
 	InitializeProgram();
 
 	glutDisplayFunc(RenderSceneCB);
--- OpenGLApp/shader.vs
+++ OpenGLApp/shader.vs
@@ -1,18 +1,14 @@
 #version 330 core
 
 layout (location = 0) in vec3 Position;
+layout (location = 1) in vec3 inColor;
 
 out vec4 Color;
 
-const vec4 colors[3] = vec4[3](
-	vec4(1,0,0,1),
-	vec4(0,1,0,1),
-	vec4(0,0,1,1)
-);
 
 uniform mat4 gMatrix;
 
 void main() {
 	gl_Position = gMatrix * vec4(Position,1.0);
-	Color = colors[gl_VertexID];
+	Color = vec4(inColor,1.0);
 }
